76f29f48f7d03a218d6529edc43b59b3a5d6d16e
Sam <samuel.nechiforel@partner.solve-it-mvi.com>
2021-08-31 09:00:34 +0300

    feat: add project base

---
+++ src/@core/core/core.module.ts
@@ -0,0 +1,30 @@
+import {LOCALE_ID, NgModule, Optional, SkipSelf} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {SignalInterceptorService} from "../../shared/utils/services/signal-interceptor.service";
+
+@NgModule({
+  imports: [HttpClientModule],
+  providers: [],
+})
+export class CoreModule {
+  constructor(
+    private _domSanitizer: DomSanitizer,
+    private _matIconRegistry: MatIconRegistry,
+  ) {
+    this._matIconRegistry.addSvgIconSetInNamespace(
+      "fa_brands",
+      this._domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/svgs/brands.svg")
+    );
+    this._matIconRegistry.addSvgIconSetInNamespace(
+      "fa_regular",
+      this._domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/svgs/regular.svg")
+    );
+    this._matIconRegistry.addSvgIconSetInNamespace(
+      "fa_solid",
+      this._domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/svgs/solid.svg")
+    );
+  }
+}
