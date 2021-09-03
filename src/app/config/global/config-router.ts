8d4215ef552c2717c8e111ff257f846151a7cea4
Sam <samuel.nechiforel@partner.solve-it-mvi.com>
2021-09-03 18:10:07 +0300

    refactor: fix the import module
    add module from app in header module
    
    yes

---
+++ src/app/config/global/config-router.ts
@@ -0,0 +1,15 @@
+export const routerArray = Object.freeze({
+  router: [
+    {
+      name: 'Pull Request',
+      path: '/pull_request'
+    } as { name: string, path: string },
+    {
+      name: 'Code',
+      path: '/code'
+    } as { name: string, path: string }, {
+      name: 'Settings',
+      path: '/settings'
+    } as { name: string, path: string }
+  ]
+})
