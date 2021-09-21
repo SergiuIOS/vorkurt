210b085831e047dff02165b10e9c1e8305e41ffa
Sam <samuel.nechiforel@partner.solve-it-mvi.com>
2021-09-21 09:14:22 +0300

    ref: another logic for spinner

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {SpinnerStateService} from "./spinner-state.service";
-import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'elix-spinner',
@@ -8,19 +7,17 @@ import {tap} from "rxjs/operators";
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent implements OnInit {
-  private _showSppiner: boolean
-
   constructor(private _spinnerStateService: SpinnerStateService) {
   }
 
-  get showSppiner(): boolean{
-    return  this._showSppiner
+  private _showSppiner: boolean
+
+  get showSppiner(): boolean {
+    return this._showSppiner
   }
 
   ngOnInit(): void {
     this._spinnerStateService.getState$
-      .pipe(tap((res) => console.log(res)))
       .subscribe(resp => this._showSppiner = resp)
   }
-
 }
